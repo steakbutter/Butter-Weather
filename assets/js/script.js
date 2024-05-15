e8ac9d81d6d6273c4bccc26e9627358329bd4f88
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-15 17:41:00 -0600

    Added city search to the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
 // This is the API key created by open weather API
 const APIKey = "34b0949fb0fbf965fbefa03f231d3872";
 
+// I want to get user input from the search city bar
+let searchBtn = document.getElementById('searchBtn');
+searchBtn.addEventListener('click', function(event){
+    event.preventDefault()
+    const cityName = document.getElementById('cityName');
+    const userSearch = {
+        city: cityName.value
+    };
+
+    let searchHistory = JSON.parse(localStorage.getItem('city'));
+    if (!searchHistory) {
+        searchHistory = []
+    }
+    searchHistory.push(userSearch)
+    localStorage.setItem('city', JSON.stringify(searchHistory));
+})
+
 // The city variable will have to be written by the user. We can retrieve it from local storage.
 let city;
+
 const queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
 fetch(queryUrl)
