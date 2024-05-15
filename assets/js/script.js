a866cb15aa829f28c49c3d403319f39b8f530feb
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-15 11:02:16 -0600

    added some comments to the script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+// This is the API key created by open weather API
 const APIKey = "34b0949fb0fbf965fbefa03f231d3872";
 
+// The city variable will have to be written by the user. We can retrieve it from local storage.
 let city;
 const queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
