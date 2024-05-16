c76a9f021c5482ae31dcd56e2399e8394a45859c
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-15 18:06:33 -0600

    Cities now display below the search bar in the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,21 @@ if(!cityData) {
 //Convert sotred data to an easier datatype
 const cityArray = JSON.parse(cityData)
 
+// Create a for loop to go through all the cities searched
+for (let i = 0; i < cityArray.length; i++) {
+    const wrapper = document.createElement('div')
+    const cityN = document.createElement('h3')
+    // Add text content to the new element created above
+    cityN.textContent = cityArray[i].city
+    // add styling to the new element
+    wrapper.classList.add("wrapperClass");
+    cityN.classList.add("cityClass");
+    //Append new element to wrapper
+    wrapper.appendChild(cityN);
+    //Add new wrapper to the DOM
+    cityContainer.append(wrapper);
+
+}
 // The city variable will have to be written by the user. We can retrieve it from local storage.
 let city;
 
