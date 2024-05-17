f11acda04d8fb06a8bdab081b614b5d475e30f6c
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-16 18:20:57 -0600

    fixed variables so API will fetch for the cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,10 @@ const APIKey = "34b0949fb0fbf965fbefa03f231d3872";
 
 // I want to get user input from the search city bar
 let searchBtn = document.getElementById('searchBtn');
-searchBtn.addEventListener('click', function(event){
+searchBtn.addEventListener('click', function (event) {
     event.preventDefault()
     const cityName = document.getElementById('cityName');
-    const userSearch = {
-        city: cityName.value
-    };
+    const userSearch = cityName.value;
 
     let searchHistory = JSON.parse(localStorage.getItem('city'));
     if (!searchHistory) {
@@ -16,15 +14,49 @@ searchBtn.addEventListener('click', function(event){
     }
     searchHistory.push(userSearch)
     localStorage.setItem('city', JSON.stringify(searchHistory));
+    let cities = cityName.value
+    const queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cities}&appid=${APIKey}`;
+
+    fetch(queryUrl, {
+        method: 'GET',
+
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
     
+        const cityContainer = document.querySelector('.cities');
+
+        const wrapper = document.createElement('div')
+        const cityN = document.createElement('p')
+        // Add text content to the new element created above
+        cityN.textContent = cities;
+        // add styling to the new element
+        wrapper.classList.add("wrapperClass");
+        cityN.classList.add("cityClass");
+        //Append new element to wrapper
+        wrapper.appendChild(cityN);
+        //Add new wrapper to the DOM
+        cityContainer.append(wrapper);
 })
+
+
 
 // Create a variable to act as the container for searched cities
 const cityContainer = document.querySelector('.cities');
+// TODO: Clear the buttons that already exist
+let oldCities = cityContainer.children;
+for (let i = 0; i < oldCities.length; i++) {
+    oldCities[i].remove();
+}
+
 
 // query data from the local storage
 const cityData = localStorage.getItem('city');
-if(!cityData) {
+if (!cityData) {
     cityData = [];
 }
 
@@ -36,7 +68,7 @@ for (let i = 0; i < cityArray.length; i++) {
     const wrapper = document.createElement('div')
     const cityN = document.createElement('p')
     // Add text content to the new element created above
-    cityN.textContent = cityArray[i].city
+    cityN.textContent = cityArray[i]
     // add styling to the new element
     wrapper.classList.add("wrapperClass");
     cityN.classList.add("cityClass");
@@ -44,21 +76,4 @@ for (let i = 0; i < cityArray.length; i++) {
     wrapper.appendChild(cityN);
     //Add new wrapper to the DOM
     cityContainer.append(wrapper);
-    
-
 }
-// The city variable will have to be written by the user. We can retrieve it from local storage.
-let city = cityData
-
-const queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
-fetch(queryUrl ,{
-    method: 'GET',
-
-})
-.then(function (response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data);
-});
